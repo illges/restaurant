5c3f7a87ff9e7742fd2eea70b7ac7993e657066a
illges <brown1ei.eb@gmail.com>
2019-01-17 12:37:29 -0500

    Initial Commit

---
+++ src/page-load.js
@@ -0,0 +1,30 @@
+//Module Pattern
+const pageLoad = (() => {
+    const render = () => {
+        const content = document.getElementById('content')
+        function createHeadLine() {
+            const div = document.createElement('div')
+            const headLine = document.createElement('h1')
+            headLine.innerHTML = 'The Silly Pickle'
+            div.appendChild(headLine)
+            return div
+        }
+
+        const image = document.createElement('img')
+        image.setAttribute('src', '../images/pickle.jpg')
+
+        const article = document.createElement('p')
+        article.innerHTML = 'Here you\'ll find the best pickle in town. On my honors as a pickleman.'
+
+        content.appendChild(createHeadLine())
+        content.appendChild(image)
+        content.appendChild(article)
+    }
+
+    return {
+        render
+    }
+})();
+
+
+export default pageLoad

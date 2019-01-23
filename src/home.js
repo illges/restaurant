b6ad6dee7a7ace7a856a7a67a896df8d17ca6537
illges <brown1ei.eb@gmail.com>
2019-01-23 07:30:37 -0500

    First Linting

--- src/home.js
+++ src/home.js
@@ -1,30 +1,30 @@
 //Module Pattern
 const displayHome = (() => {
-    const render = () => {
-        const content = document.getElementById('content')
-        function createHeadLine() {
-            const div = document.createElement('div')
-            const headLine = document.createElement('h1')
-            headLine.innerHTML = 'The Silly Pickle'
-            div.appendChild(headLine)
-            return div
-        }
-
-        const image = document.createElement('img')
-        image.setAttribute('src', '../images/pickle.jpg')
-
-        const article = document.createElement('p')
-        article.innerHTML = 'Here you\'ll find the best pickle in town. On my honors as a pickleman.'
-
-        content.appendChild(createHeadLine())
-        content.appendChild(image)
-        content.appendChild(article)
+  const render = () => {
+    const content = document.getElementById("content");
+    function createHeadLine() {
+      const div = document.createElement("div");
+      const headLine = document.createElement("h1");
+      headLine.innerHTML = "The Silly Pickle";
+      div.appendChild(headLine);
+      return div;
     }
 
-    return {
-        render
-    }
+    const image = document.createElement("img");
+    image.setAttribute("src", "../images/pickle.jpg");
+
+    const article = document.createElement("p");
+    article.innerHTML =
+      "Here you'll find the best pickle in town. On my honors as a pickleman.";
+
+    content.appendChild(createHeadLine());
+    content.appendChild(image);
+    content.appendChild(article);
+  };
+
+  return {
+    render
+  };
 })();
 
-
-export default displayHome
+export default displayHome;
